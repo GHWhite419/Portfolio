142c634f7433dad048d531d12b6dba078c69888c
Galen White <galenhwhite419@gmail.com>
2023-03-18 23:53:11 -0400

    Modal functionality working successfully; trying to manage a toggle effect on each button.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// Slideshow/Carousel
 const slideShow = document.querySelectorAll('section');
-console.log(slideShow);
 
 const prevSlide = document.querySelector('.prevSlide');
 const nextSlide = document.querySelector('.nextSlide');
@@ -11,11 +11,10 @@ slideShow.forEach((slide, index) => {
 let currentSlide = 0;
 let lastSlide = slideShow.length - 1;
 
-prevSlide.addEventListener('click', function(){
+prevSlide.addEventListener('click', function () {
     if (currentSlide != 0) {
         currentSlide--;
     }
-    console.log(currentSlide);
 
     slideShow.forEach((slide, index) => {
         slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
@@ -23,13 +22,33 @@ prevSlide.addEventListener('click', function(){
     })
 })
 
-nextSlide.addEventListener('click', function(){
+nextSlide.addEventListener('click', function () {
     if (currentSlide != lastSlide) {
         currentSlide++;
     }
-    
+
     slideShow.forEach((slide, index) => {
         slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
         slide.style.transition = '.3s';
     })
 })
+
+
+// Project modal popups
+const projectButton = document.querySelectorAll('.projectButton');
+const projectModal = document.querySelectorAll('.projectModal');
+
+projectButton.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const index = Array.from(projectButton).indexOf(e.target);
+        projectModal.forEach(modal => {
+                modal.classList.add('modalHidden');
+        })
+        const targetModal = projectModal[index];
+        targetModal.classList.toggle('modalHidden');
+    })
+});
+
+// The forEach loop adds the display none proeprty on each modal.
+// Then the targetModal toggle removes the display property from the corresponding modal.
+// I want a conditional that prevents the specified modal from having its displaly none property removed if its already opened.
