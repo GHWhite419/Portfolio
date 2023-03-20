3934570e05aef3af2e2bdc49a30d3aa0ac22aae3
Galen White <galenhwhite419@gmail.com>
2023-03-20 01:20:59 -0400

    Bout time for a commit. Color changer, email form clear, and nav menu all complete.

--- script.js
+++ script.js
@@ -14,23 +14,32 @@ let lastSlide = slideShow.length - 1;
 prevSlide.addEventListener('click', function () {
     if (currentSlide != 0) {
         currentSlide--;
+        openMenu.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
+        linkedIn.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
+        gitHub.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
     }
 
     slideShow.forEach((slide, index) => {
         slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
         slide.style.transition = '.3s';
+        // openMenu.classList.remove('introColor, aboutColor, projectsColor, contactColor');
     })
+    slideColor();
 })
 
 nextSlide.addEventListener('click', function () {
     if (currentSlide != lastSlide) {
         currentSlide++;
+        openMenu.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
+        linkedIn.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
+        gitHub.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
     }
 
     slideShow.forEach((slide, index) => {
         slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
         slide.style.transition = '.3s';
     })
+    slideColor();
 })
 
 
@@ -58,3 +67,69 @@ closeModal.forEach(closeButton => {
         targetModal.classList.add('modalHidden');
     })
 })
+
+
+// Nav menu
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const navMenu = document.querySelector('.navMenu');
+openMenu.addEventListener('click', () => {
+    navMenu.classList.add('navOpen');
+})
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('navOpen');
+})
+
+const links = document.querySelectorAll('.navMenu li');
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        const index = Array.from(links).indexOf(e.target);
+        currentSlide = index + 1;
+        navMenu.classList.remove('navOpen');
+        openMenu.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
+        linkedIn.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
+        gitHub.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
+        slideShow.forEach((slide, index) => {
+            slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+            slide.style.transition = '.3s';
+        })
+        slideColor();
+    })
+})
+
+// Dynamic hover styles
+const linkedIn = document.querySelector('footer .fa-linkedin');
+const gitHub = document.querySelector('footer .fa-github');
+const slideColor = () => {
+    switch (currentSlide) {
+        case 0:
+            openMenu.classList.add('introColor');
+            linkedIn.classList.add('introColor');
+            gitHub.classList.add('introColor');
+            break;
+        case 1:
+            openMenu.classList.add('aboutColor');
+            linkedIn.classList.add('aboutColor');
+            gitHub.classList.add('aboutColor');
+            break;
+        case 2:
+            openMenu.classList.add('projectsColor');
+            linkedIn.classList.add('projectsColor');
+            gitHub.classList.add('projectsColor');
+            break;
+        case 3:
+            openMenu.classList.add('contactColor');
+            linkedIn.classList.add('contactColor');
+            gitHub.classList.add('contactColor');
+            break;
+    }
+}
+slideColor();
+
+
+// Clear email form
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName('form')) {
+        form.reset();
+    }
+}
