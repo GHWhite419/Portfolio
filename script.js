abee0a9953525be751f71a88d3486382ee1e5e53
Galen White <galenhwhite419@gmail.com>
2023-10-03 00:46:53 -0400

    Continuing with javascript commentary; completed commentary on previous and next slide buttons

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ slideShow.forEach((slide, index) => {
 // First I declare currentSlide as 0, simply to initialize this process. I also declare lastSlide as the fourth section using slideShow's length so that I have a clearly defined boundary.
 let currentSlide = 0;
 let lastSlide = slideShow.length - 1;
-// Lastly, this function adds a "sliderDisable" class to the navigation buttons if our current slide is either the first or last section. This class dulls out the colors to indicate their status as being un-usable (I will need to tweak its accessibility features so it's not selectable at all.)
+
+// Lastly, this function adds a "sliderDisable" class to the navigation buttons if our current slide is either the first or last section. This class dulls out the colors to indicate their status as being un-usable (I will need to tweak its accessibility features so it's not selectable at all). 
 const disableButton = () => {
     if (currentSlide === 0) {
         prevSlide.classList.add('sliderDisable');
@@ -27,11 +28,17 @@ const disableButton = () => {
         nextSlide.classList.remove('sliderDisable');
     }
 }
+// After defining this function, I simply need to initialize it with this line below so that it disables the "previous" button when we start on the first button.
 disableButton();
 
+// Now I will add event listeners to the two navigation buttons, both of which activate when clicked. I begin with "previous".
 prevSlide.addEventListener('click', function () {
+    // The if gate ensures that the function only performs when the user is not on the first slide. Between the function's inactivity and the "previous" button being greyed out, this creates the user-experience of the button being "disabled" so they cannot navigate backwards to before the first slide.
     if (currentSlide != 0) {
+        // This line simply decrements the currentSlide variable, which is called in a function below to shift the slides backwards.
         currentSlide--;
+        // The following classList methods here contribute to an effect that ensures all the hover states on the page match the main color of the active slide. For example, blue is used as the primary color on the "Projects" page, therefore, while the user is on the Projects slide, all the hoverable icons should appear blue when hovered over.
+        // The way these methods contribute to that effect is to remove the color from each hoverable icon when the slide is changed. A function created later will then re-add colors to the hover states. Note that this code can definitely benefit from some re-factoring.
         openMenu.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
         linkedIn.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
         gitHub.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
@@ -39,17 +46,25 @@ prevSlide.addEventListener('click', function () {
         nextSlide.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
     }
 
+    // This forEach loop is where the magic happens in the slideshow. We use the style.transform method to target each slide, with currentSlide as mentioned above being used as an argument in the translateX value. Subtracting currentSlide from the index argument tells the loop which slide we want to display as the active slide, which matches up with currentSlide's value.
+    // I also add a simple .3s transition for style.
     slideShow.forEach((slide, index) => {
         slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
         slide.style.transition = '.3s';
     })
+    // Completing the effect, I re-declare slideColor (detailed below) to re-color the hoverable buttons, and disableButton to check if the "previous" button should be disabled.
     slideColor();
     disableButton();
 })
 
+// The "next" button is the same exact principle, naturally with some slight alterations.
 nextSlide.addEventListener('click', function () {
+    // Differing from the "previous" button's if gate, here we check to ensure that we're not on the last slide so that the code can run. Otherwise, the "next" button shouldn't run its function.
     if (currentSlide != lastSlide) {
+        // Instead of decrementing currentSlide, we increment it so that all slides move forward.
         currentSlide++;
+
+        // The entire rest of the codeblock is identical to what's described in the "previous" button event listener.
         openMenu.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
         linkedIn.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
         gitHub.classList.remove('introColor', 'aboutColor', 'projectsColor', 'contactColor');
